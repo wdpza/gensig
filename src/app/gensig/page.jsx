480530960fec6fac57bd0d27f11d3d6531c91b62
wdpza <simplexityza@gmail.com>
2023-10-03 14:12:27 +0200

    layout update

--- src/app/gensig/page.jsx
+++ src/app/gensig/page.jsx
@@ -6,7 +6,7 @@ import SignatureGenerator from '@/components/SignatureGenerator'
 export default function GenSig() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+      <div className="flex flex-col z-10 max-w-5xl w-full items-center justify-between font-mono text-sm">
         <h1 className='text-center text-4xl mb-5'>Generate Signature</h1>
         <SignatureGenerator />
       </div>
